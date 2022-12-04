79778b532c96d5ee425ad487f64faf157ad97592
BroderViktor <viktor.asheim@gmail.com>
2022-12-04 23:13:15 +0100

    added line rendering and lines between particles

--- src/pages/BackgroundGraphics.jsx
+++ src/pages/BackgroundGraphics.jsx
@@ -6,7 +6,7 @@ import TestObject from "../assets/test.obj";
 import Titlecard from "../assets/plane.obj";
 import { useImperativeHandle } from "react";
 
-const particlesNum = 1100;
+const particlesNum = 200;
 const speed = 1;
 
 async function loadObjData(obj, scale) {
@@ -26,11 +26,11 @@ async function loadObjData(obj, scale) {
 class particle {
 
 
-    constructor(position, velocity, acceleration, octree, viewport) {
+    constructor(id, position, velocity, acceleration, octree, viewport) {
         this.xChunkOld = -100;
         this.yChunkOld = -100;
         this.zChunkOld = -100;
-
+        this.id = id;
         this.position = new THREE.Vector3(0);
         this.SetPosition(octree, position, viewport, true)
         this.originalPosition = this.position;
@@ -38,6 +38,10 @@ class particle {
         this.acceleration = acceleration;
         this.targetPos = new THREE.Vector3(0);
         this.active = false;
+
+        this.activeLine = false;
+        this.line = -1;
+        this.boundParticle = null;
     }
     goToWanted() {
         let dir = new THREE.Vector3(0);  
@@ -48,12 +52,12 @@ class particle {
         this.velocity.add(dir.multiplyScalar(this.acceleration));
     }
     SetPosition(octree, newPos, viewport, init = false) {
-        let xChunk = Math.floor((newPos.x / (viewport.width + 10)) * 4) + 2;
-        let yChunk = Math.floor((newPos.y / (viewport.height + 10)) * 4) + 2;
-        let zChunk = Math.floor((newPos.z / (viewport.height + 10)) * 4) + 2;
+        let xChunk = Math.floor((newPos.x / (viewport.width + 10)) * octree.x + octree.x/2);
+        let yChunk = Math.floor((newPos.y / (viewport.height + 10)) * octree.y + octree.y/2);
+        let zChunk = Math.floor((newPos.z / (viewport.height + 10)) * octree.z + octree.z/2);
         
 
-        if (xChunk < 0 || xChunk > 3 || yChunk < 0 || yChunk > 3 || zChunk < 0 || zChunk > 3) { 
+        if (xChunk < 0 || xChunk > octree.x - 1 || yChunk < 0 || yChunk > octree.y - 1 || zChunk < 0 || zChunk > octree.z - 1) { 
             if (!this.active) {
                 //console.log("fak")
             }
@@ -147,7 +151,7 @@ class StateManager {
         this.stateMods.push()
     }
     
-    ChangeState() {
+    changeState() {
         if (this.stateArray[this.currentState] == null) {
             this.particles.forEach(particle => {
                 particle.velocity = new THREE.Vector3((Math.random() - 0.5) * speed, (Math.random() - 0.5) * speed, (Math.random() - 0.5) * speed);
@@ -166,12 +170,89 @@ class StateManager {
     }
 }
 
+class MeshManager {
+    
+    constructor(maxLines) {
+        this.linePositions = new Float32Array(maxLines * 3 * 4);
+        this.indices = new Uint16Array(maxLines * 3 * 2)
+
+        this.openSpots = []
+        this.fillSpot = 0
+    }
+    addLine(vec1, vec2, width) {
+        if (this.fillSpot * 3 * 4 == this.linePositions) return;
+
+        let veca = vec1.x + vec1.y > vec2.x + vec2.y ? vec1 : vec2;
+        let vecb = vec1.x + vec1.y < vec2.x + vec2.y ? vec1 : vec2;
+
+        //vert 0
+        this.linePositions[this.fillSpot * 3 * 4 + 0] = veca.x - width;
+        this.linePositions[this.fillSpot * 3 * 4 + 1] = veca.y;
+        this.linePositions[this.fillSpot * 3 * 4 + 2] = veca.z;
+
+        this.linePositions[this.fillSpot * 3 * 4 + 3] = veca.x + width;
+        this.linePositions[this.fillSpot * 3 * 4 + 4] = veca.y;
+        this.linePositions[this.fillSpot * 3 * 4 + 5] = veca.z;
+       
+        //vert 1
+        this.linePositions[this.fillSpot * 3 * 4 + 6] = vecb.x - width;
+        this.linePositions[this.fillSpot * 3 * 4 + 7] = vecb.y;
+        this.linePositions[this.fillSpot * 3 * 4 + 8] = vecb.z;
+
+        this.linePositions[this.fillSpot * 3 * 4 + 9] = vecb.x + width;
+        this.linePositions[this.fillSpot * 3 * 4 + 10] = vecb.y;
+        this.linePositions[this.fillSpot * 3 * 4 + 11] = vecb.z;
+
+        this.indices[this.fillSpot * 3 * 2 + 0] = [this.fillSpot * 4 + 0];
+        this.indices[this.fillSpot * 3 * 2 + 1] = [this.fillSpot * 4 + 2];
+        this.indices[this.fillSpot * 3 * 2 + 2] = [this.fillSpot * 4 + 3];
+
+        this.indices[this.fillSpot * 3 * 2 + 3] = [this.fillSpot * 4 + 0];
+        this.indices[this.fillSpot * 3 * 2 + 4] = [this.fillSpot * 4 + 3];
+        this.indices[this.fillSpot * 3 * 2 + 5] = [this.fillSpot * 4 + 1];
+
+        this.fillSpot += 1;
+        return (this.fillSpot - 1);
+    }   
+    updateLine(vec1, vec2, width, index) {
+        
+        if (index >= this.fillSpot) return;
+
+        let veca = vec1.x + vec1.y > vec2.x + vec2.y ? vec1 : vec2;
+        let vecb = vec1.x + vec1.y < vec2.x + vec2.y ? vec1 : vec2;
+
+        //vert 0
+        this.linePositions[index * 3 * 4 + 0] = veca.x - width;
+        this.linePositions[index * 3 * 4 + 1] = veca.y;
+        this.linePositions[index * 3 * 4 + 2] = veca.z;
+
+        this.linePositions[index * 3 * 4 + 3] = veca.x + width;
+        this.linePositions[index * 3 * 4 + 4] = veca.y;
+        this.linePositions[index * 3 * 4 + 5] = veca.z;
+       
+        //vert 1
+        this.linePositions[index * 3 * 4 + 6] = vecb.x - width;
+        this.linePositions[index * 3 * 4 + 7] = vecb.y;
+        this.linePositions[index * 3 * 4 + 8] = vecb.z;
+
+        this.linePositions[index * 3 * 4 + 9] = vecb.x + width;
+        this.linePositions[index * 3 * 4 + 10] = vecb.y;
+        this.linePositions[index * 3 * 4 + 11] = vecb.z;
+
+        this.indices[index * 3 * 2 + 0] = [index * 4 + 0];
+        this.indices[index * 3 * 2 + 1] = [index * 4 + 2];
+        this.indices[index * 3 * 2 + 2] = [index * 4 + 3];
+
+        this.indices[index * 3 * 2 + 3] = [index * 4 + 0];
+        this.indices[index * 3 * 2 + 4] = [index * 4 + 3];
+        this.indices[index * 3 * 2 + 5] = [index * 4 + 1];
+    }   
+}
+
 const PointManager = React.forwardRef((props, ref) => {
     const {viewport} = useThree();
     const pointManager = useRef();
-    
-
-
+    const meshBuffer = useRef();
 
     function repeatingState() {
         setTimeout(() => {
@@ -205,30 +286,75 @@ const PointManager = React.forwardRef((props, ref) => {
 
     const {particles, octree, stateManager} = useMemo(() => {
         
-        let octree = new worldOctree(4,4,4)
+        let octree = new worldOctree(1,1,1)
 
         let particles = new Array(particlesNum);
         for (let i = 0; i < particles.length; i++) {
             let pos = new THREE.Vector3((Math.random() - 0.5) * viewport.width, (Math.random() - 0.5) * viewport.height, 0)
             let vel = new THREE.Vector3((Math.random() - 0.5) * speed, (Math.random() - 0.5) * speed, (Math.random() - 0.5) * speed)
-            particles[i] = new particle(pos, vel, 0.05, octree, viewport)
+            particles[i] = new particle(i, pos, vel, 0.05, octree, viewport)
         }
 
 
         //repeatingState();
         let stateManager = new StateManager(particles);
         stateManager.loadStates();
+        
+
         return {particles, octree, stateManager}; 
     }, [])
 
     
 
-    useFrame(() => {
+    useFrame(({clock}) => {
         //octree.runCheck();
+
+        //console.log(meshManager.fillSpot)
         for (let i = 0; i < particles.length; i++) {
             if (particles[i].active) {
                 particles[i].goToWanted()
             }
+
+            if (!particles[i].activeLine) {
+                let closeParticles = octree.octree[particles[i].xChunkOld][particles[i].yChunkOld][particles[i].zChunkOld];
+                let closestDistance = Infinity;
+                let closestParticle = null;
+                closeParticles.forEach((particle) => {
+                    if (particles[i].position == particle.position || particle.activeLine) return;
+                    
+                    let dis = particles[i].position.distanceToSquared(particle.position);
+                    if (dis < closestDistance) { 
+                        closestDistance = dis; 
+                        closestParticle = particle;
+                    }
+                })
+                let r = Math.random() > 0.9;
+
+                if (closestDistance < 2000 && r) {
+                    particles[i].activeLine = true;
+                    particles[i].boundParticle = closestParticle;
+                    particles[i].line = meshManager.addLine(particles[i].position, closestParticle.position, 0.1);
+                    closestParticle.activeLine = true;
+                    closestParticle.boundParticle = particles[i];
+                    closestParticle.line = particles[i].line;
+                }
+            }
+            else {
+                meshManager.updateLine(particles[i].position, particles[i].boundParticle.position, 0.1, particles[i].line)
+                if (particles[i].position.distanceToSquared(particles[i].boundParticle.position) > 2000) {
+                    meshManager.updateLine(new THREE.Vector3(-1000, -1000, 1000), new THREE.Vector3(-1000, -1000, 1000), 0.0,  particles[i].line);
+                    
+                    particles[i].activeLine = false;
+                    particles[i].line = -1;
+                    
+                    particles[i].boundParticle.activeLine = false;
+                    particles[i].boundParticle.boundParticle = null;
+                    particles[i].boundParticle.line = -1;
+
+                    particles[i].boundParticle = null;
+                }
+            }
+            
 
             particles[i].SetPosition(octree, particles[i].position.add(particles[i].velocity), viewport);
 
@@ -243,16 +369,62 @@ const PointManager = React.forwardRef((props, ref) => {
             pointManager.current.setMatrixAt(i, transform)
             
         }
+        meshBuffer.current.attributes.position.needsUpdate = true;
         pointManager.current.instanceMatrix.needsUpdate = true
         pointManager.current.instanceColor.needsUpdate = true
     })
+        
+    const meshManager = new MeshManager(100);
+
 
     return (
+<>
         <instancedMesh ref={pointManager} args={[null,null,particlesNum]}>
-            <circleGeometry args={[1]}/>
+            <circleGeometry args={[0.8]}/>
             <meshStandardMaterial color={[1,1,1]}/>
         </instancedMesh>
+        
+        <mesh>
+            <bufferGeometry ref={meshBuffer}>
+                <bufferAttribute 
+                    attach={"attributes-position"} 
+                    array={meshManager.linePositions} 
+                    count={meshManager.linePositions.length / 4}
+                    itemSize={3}/>
+                <bufferAttribute 
+                    attach={"index"} 
+                    array={meshManager.indices} 
+                    count={meshManager.indices.length}
+                    itemSize={1}/>
+
+                <meshStandardMaterial
+                    vertexColors
+                    side={THREE.BackSide}/>
+            </bufferGeometry>
+        </mesh>
+</>
+
+
     )
+    //
+
+
+    /*<line ref={ref} geometry={lineGeometry}>
+                <lineBasicMaterial attach="material" color={'#9c88ff'} linewidth={10} linecap={'round'} linejoin={'round'} />
+            </line>) */
+    //<meshStandardMaterial color={[1,1,1]}/>*/
+
+    /*        
+    <instancedMesh ref={pointManager} args={[null,null,1]}>
+        
+    </instancedMesh>
+            <mesh>
+
+            <circleGeometry args={[0.8]}/>
+            <meshStandardMaterial color={[1,1,1]}/>
+
+        </mesh>
+    */
 })
 
 export default function Background() {
@@ -267,6 +439,7 @@ export default function Background() {
                 </Canvas>
             </div>
             <button onClick={() => PointManagerRef.current.GetStateManager().ChangeState()}>Change State</button>
+            <button onClick={() => window.location.reload(false)}>update State</button>
         </>
     )
 }
